8e5ba58b84ed93eadca951ebfe00ebf35bfb54e8
Florian Jud <florian.jud@famjud.de>
2021-11-14 14:35:31 +0100

    Added Routing to Categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
+import { CategoriesComponent } from './categories/categories.component'
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "home" },
   { path: "home", component: RecipesComponent },
   { path: "add", component: AddRecipeComponent },
-  { path: "recipe/:id", component: DetailComponent }
+  { path: "recipe/:id", component: DetailComponent },
+  { path: "categories", component: CategoriesComponent }
 ];
 
 @NgModule({
