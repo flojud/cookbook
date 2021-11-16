b77fe3c605c34a17dd5850c04af18a119599b211
Florian Jud <florian.jud@famjud.de>
2021-11-16 20:11:02 +0100

    Added action button

--- src/app/action-button/action-button.component.ts
+++ src/app/action-button/action-button.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { actionButtonAnimations } from './action-button.animations';
+import { RecipesService } from '../services/recipes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-action-button',
@@ -9,13 +11,42 @@ import { actionButtonAnimations } from './action-button.animations';
   animations: actionButtonAnimations
 })
 export class ActionButtonComponent implements OnInit {
+  
+  public isRecipePage: boolean = false;
+  private recipeID: String;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(
+    private router: Router,
+    public dialog: MatDialog,
+    private recipesService: RecipesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  buttonClick() {
+    this.isRecipePage = this.getRecipeIdFromUrl(window.location.pathname);
   }
 
-  doAction(val: string) {
-    console.log(val);
+  getRecipeIdFromUrl(url: string){
+    if(url.includes('/recipe')){
+      const urlArray = url.split('/');
+      this.recipeID = urlArray[2];
+      return true;
+    }else{
+      return false;
+    }
   }
+
+  edit(){
+    this.router.navigate(['/recipe/'+this.recipeID+'/edit']);
+  }
+
+  delete(){
+    if(confirm("Möchtest du das Rezepte " + this.recipeID + " wirklich löschen?")) {
+      this.recipesService.deleteRecipe(this.recipeID);
+    } 
+  }
+  bookmark(){}
+  share(){}
+
+
 }
