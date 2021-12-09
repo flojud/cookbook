dc8034884942b0434fd142d6558c9ce3308b6380
Florian Jud <florian.jud@famjud.de>
2021-12-09 19:55:55 +0100

    Category edit and delete button

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../models/category';
 import { CategoryService } from '../services/category.service';
+import { CurrentUserService } from '../services/currentUser.service';
 
 @Component({
   selector: 'app-categories',
@@ -8,14 +9,21 @@ import { CategoryService } from '../services/category.service';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  public isLoggedIn: boolean = false;
   
   categories: Category[] = [];
-  constructor(private categoryService: CategoryService) {
+  constructor(private categoryService: CategoryService,
+    private currentUserService: CurrentUserService) {
     this.categoryService.getCategories().subscribe( res => {
       this.categories = res;
     })
   }
   
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isLoggedIn = this.currentUserService.isLoggedIn();
+  }
+
+  edit(){}
+  delete(){}
 
 }
